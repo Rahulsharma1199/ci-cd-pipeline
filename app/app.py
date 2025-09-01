5bfeace6dac79727cccdc8106eb661a8ec15e314
Rahulsharma1199 <rahul.sharma.ag99@gmail.com>
2025-09-01 21:01:08 +0530

    update8

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask (__name__)
 
 @app.get("/")
 def hello():
-    return jsonify(message="Hello from Flask CI/CD")
+    return jsonify(message="Hello welcome to from Flask CI/CD")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
